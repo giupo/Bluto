6c7474bd686de4e9f8ac1bde43ea02c3cfecc25a
Giuseppe Acito <giuseppe.acito@gmail.com>
2022-03-01 07:17:26 -0500

    Bump version: 2.4.18 → 2.4.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bluto',
-    version='2.4.18',
+    version='2.4.19',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/Bluto',
