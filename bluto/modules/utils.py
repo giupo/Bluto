77cd5d4daf9f2b010f21923f13ad133f2446cfb5
Giuseppe Acito <giuseppe.acito@gmail.com>
2022-03-01 10:18:23 -0500

    adds some helpers function to mitigate copy/pasting code

--- bluto/modules/utils.py
+++ bluto/modules/utils.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
+import shutil
+import webbrowser
+
 def yes_or_no():
     """Helper function to handle user input for yes or no"""
     while True:
@@ -9,3 +13,30 @@ def yes_or_no():
             print(f"\nOptions are yes|no or y|n, Not '{user_input}'")
         else:
             return answer in ('y', 'yes')
+
+
+def remove_reports(docs, log_dir, report_location):
+    """Removes reports retrieved"""
+    shutil.rmtree(docs)
+    shutil.rmtree(log_dir)
+    os.remove(report_location)
+
+
+def handle_local_data(docs, log_dir, report_location):
+    """
+    helper function for interactively handle local data persistence
+    """
+    print("\nWould you like to keep all local data?\n")
+    print("(Local Logs, Downloded Documents, HTML Evidence Report)")
+    if yes_or_no():
+        print(f"\nThe documents are located here: {docs}")
+        print(f"The logs are located here: {log_dir}.")
+        print(f"\nAn evidence report has been written to {report_location}\n")
+        print("Would you like to open this report now?\n")
+        if yes_or_no():
+            print(f'\nOpening {report_location}')
+            webbrowser.open('file://{report_location}')
+    else:
+        print("removing local data...")
+        remove_reports(docs, log_dir, report_location)
+        print("done removing local data.")
